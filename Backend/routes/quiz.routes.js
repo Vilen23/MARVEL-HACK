aeed82a66aa6b9e91ad29467bbc6fa17f89acc51
Shivam <shivamlather5@gmail.com>
2024-03-13 20:02:38 +0530

    Added authentication to getquiz

--- Backend/routes/quiz.routes.js
+++ Backend/routes/quiz.routes.js
@@ -4,6 +4,6 @@ const {quizscoreee, getquiz} = require('../controllers/quiz.js');
 const { verifyToken } = require('../middlewares/usercheck.js');
 
 router.post('/submit',quizscoreee);
-router.get('/quizquestion',getquiz);
+router.get('/quizquestion',verifyToken,getquiz);
 
 module.exports = router;
